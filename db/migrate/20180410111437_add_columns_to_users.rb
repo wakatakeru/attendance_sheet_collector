6ba4867771a0cd4d7b841f0c6afafc1623f0f2ce
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-11 13:06:09 +0900

    データベースの一意性制約などを追加

--- db/migrate/20180410111437_add_columns_to_users.rb
+++ db/migrate/20180410111437_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :st_num, :string
+    add_column :users, :st_num, :string, unique: true
     add_column :users, :name, :string
     add_column :users, :role, :int
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#== TestUser
+User.create(
+  name: 'hoge',
+  st_num: 's00t200',
+  email: 's00t200@stu.kagawa-u.ac.jp',
+  role: 10,
+  password: 'hogehoge',
+  password_confirmation: 'hogehoge'
+)
