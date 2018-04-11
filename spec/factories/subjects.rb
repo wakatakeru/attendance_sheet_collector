f6730182c34529b094e3d2fd63d78c13801c99ff
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-11 18:40:05 +0900

    subjectのファクトリを作成

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,8 +1,18 @@
+require 'faker'
+
 FactoryBot.define do
   factory :subject do
-    name "MyString"
-    display_name "MyString"
-    term ""
-    url "MyString"
+    name { Faker::Name.name }
+    display_name { Faker::Name.name }
+    term '2099'
+    url 'http://example.com'
+    
+    trait :no_name do
+      name ''
+    end
+
+    trait :no_term do
+      term ''
+    end
   end
 end
