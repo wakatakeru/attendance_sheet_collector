41c5ffeae963558fe20abbac292453a3200f1b57
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-11 18:54:57 +0900

    Subjectのモデルについてバリデーションテストのスケルトンを作成

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション確認' do
+    context '登録を試行したとき' do
+      it '授業と年度が入っていれば登録成功' do
+
+      end
+      
+      it '重複していれば登録が失敗' do
+
+      end
+
+      it '授業名が空なら登録が失敗' do
+
+      end
+
+      it '年度が空なら登録が失敗' do
+
+      end
+    end
+  end
 end
