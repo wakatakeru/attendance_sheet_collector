2b288eae275ef5d9bc43523b712ba09589b5d218
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-11 13:19:26 +0900

    学籍番号などのユーザ名でログインできるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:st_num, :password)}
+  end
 end
