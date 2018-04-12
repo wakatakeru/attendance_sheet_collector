f2ead5b4963350d3130de670b04ff08abb5bdcb9
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-12 19:21:12 +0900

    subjectモデルのテストを記述

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -2,17 +2,9 @@ require 'faker'
 
 FactoryBot.define do
   factory :subject do
-    name { Faker::Name.name }
+    name 'TestExpr1'
     display_name { Faker::Name.name }
     term '2099'
     url 'http://example.com'
-    
-    trait :no_name do
-      name ''
-    end
-
-    trait :no_term do
-      term ''
-    end
   end
 end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,22 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
+  let(:subject) { FactoryBot.create(:subject) }
+
   describe 'バリデーション確認' do
     context '登録を試行したとき' do
-      it '授業と年度が入っていれば登録成功' do
-
+      it '授業と年度が重複していなければ登録成功' do
+        s = FactoryBot.build(:subject, name: 'TestExpr2', term: '2000')
+        expect(s).to be_valid
       end
       
-      it '重複していれば登録が失敗' do
-
+      it '年度が重複していれば登録失敗' do
+        s = FactoryBot.build(:subject, term: subject.term)
+        expect(s).not_to be_valid
       end
 
-      it '授業名が空なら登録が失敗' do
-
+      it '授業名が重複していれば登録失敗' do
+        s = FactoryBot.build(:subject, name: subject.name)
+        expect(s).not_to be_valid
       end
 
-      it '年度が空なら登録が失敗' do
+      it '授業名が空なら登録失敗' do
+        s = FactoryBot.build(:subject, name: '')
+        expect(s).not_to be_valid
+      end
 
+      it '年度が空なら登録失敗' do
+        s = FactoryBot.build(:subject, term: '')
+        expect(s).not_to be_valid
       end
     end
   end
