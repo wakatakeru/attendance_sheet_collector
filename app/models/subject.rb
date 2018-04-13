32b9ab378be898e9d8a772c2fb2aa988ec4dc356
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2018-04-13 15:41:06 +0900

    指摘事項を修正

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,6 +3,6 @@ class Subject < ApplicationRecord
   has_and_belongs_to_many :users
 
   validates :name, presence: true, uniqueness: true
-  validates :display_name, uniqueness: true
+  validates :display_name, presence: true, uniqueness: true
   validates :term, presence: true
 end
